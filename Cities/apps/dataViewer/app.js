9b53af26095f5090e4792b6926f29bc1ab8ac01d
wilk666 <andre@cartomatic.pl>
2014-07-25 16:33:38 +0200

    Work on gadget

--- Cities/apps/dataViewer/app.js
+++ Cities/apps/dataViewer/app.js
@@ -1,6 +1,7 @@
 ﻿Ext.Loader.setConfig({
 	enabled:true
 });
+
 // NOTE: important to give valid not relative path - it has to be available online
 // as when rendered by OpenSocial Container it will get it's url
 //The actual app
@@ -12,7 +13,6 @@ var __localhost__ = 'http://localhost/';
 //ExtJs extensions
 Ext.Loader.setPath('Ext.ux', __localhost__ + 'jslibs/ExtJs/' + __extjs__ + '/examples/ux');
 
-
 Ext.require([
 	'Gnx.AppLogic'
 ]);
@@ -20,10 +20,8 @@ Ext.require([
 Ext.application({
 	name: 'gnx',
 	launch: function () {
-
-	    Ext.Msg.alert('DUPA', '666');
 	    Ext.QuickTips.init();
 	    Ext.create('Gnx.AppLogic');
-
 	}
 });
+
